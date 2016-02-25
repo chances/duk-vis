80799f47f5bd7733546398b5c0365cf8c1ab3c5c
Chance Snow <git@chancesnow.me>
2016-02-25 01:28:50 -0800

    Remove bogus OOGL shims

--- cmake/oogl.cmake
+++ cmake/oogl.cmake
@@ -2,11 +2,6 @@
 
 include_directories(vendor/OOGL/include)
 
-list(APPEND SOURCE_FILES
-    # sources/Window_GLFW.cpp
-    sources/Context_GLFW.cpp
-)
-
 # Link with OOGL
 set(OOGL_LIBRARY ${CMAKE_SOURCE_DIR}/vendor/OOGL/lib/OOGL.a)
 list(APPEND LINK_LIBS ${OOGL_LIBRARY})
