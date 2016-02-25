85715b1bc66b2bd411f2948c376b3e94b5e327ab
Chance Snow <git@chancesnow.me>
2016-02-25 01:31:46 -0800

    Cleanup rxcpp cmake include

--- cmake/rxcpp.cmake
+++ cmake/rxcpp.cmake
@@ -1,12 +1,3 @@
 # Include RxCpp
 
 include_directories(vendor/RxCpp/Rx/v2/src)
-
-# list(APPEND SOURCE_FILES
-#     # sources/Window_GLFW.cpp
-#     sources/Context_GLFW.cpp
-# )
-
-# # Link with OOGL
-# set(OOGL_LIBRARY ${CMAKE_SOURCE_DIR}/vendor/OOGL/lib/OOGL.a)
-# list(APPEND LINK_LIBS ${OOGL_LIBRARY})
